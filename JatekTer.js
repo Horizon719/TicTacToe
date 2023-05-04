f79a887aeb6320fe2e38881f2a6dc1fd7ba0e0b0
Horizon719 <milichovszky.sandor@gmail.com>
2023-05-04 15:47:09 +0200

    JatekTer.js javítása

--- JatekTer.js
+++ JatekTer.js
@@ -11,21 +11,20 @@ class JatekTer {
         const TAROLO = $("#tarolo");
         for (let i = 0; i < 9; i++) {
             const ELEM = new Elem(TAROLO, i);
-
-            $(window).on("elemKattintas", (event) =>{
-                if (event.detail.P.html() != "") {
-                    return;
-                }
-                if (this.#lepes % 2 === 0) {
-                    event.detail.setElem("X");
-                    this.#jatekterLista[event.detail.id] = "X";
-                } else {
-                    event.detail.setElem("O");
-                    this.#jatekterLista[event.detail.id] = "O";
-                }
-                this.#lepes++;
-            });
         }
+        $(window).on("elemKattintas", (event) =>{
+            if (event.detail.P.html() != "") {
+                return;
+            }
+            if (this.#lepes % 2 === 0) {
+                event.detail.setElem("X");
+                this.#jatekterLista[event.detail.id] = "X";
+            } else {
+                event.detail.setElem("O");
+                this.#jatekterLista[event.detail.id] = "O";
+            }
+            this.#lepes++;
+        });
     }
 }
 export default JatekTer
